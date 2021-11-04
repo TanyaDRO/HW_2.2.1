1cbf31f9640a531bbe9ce3bc65cf21d52b750bc0
TanyaDRO <drtanja@yandex.ru>
2021-11-04 13:44:46 +0300

    second commit

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,9 +1,8 @@
 public class BonusMilesService {
     public int calculate(int cost) {
-        int ticket = 1_000;
         int mile = 20;
-        int bonus = ticket / mile;
+        int miles = cost / mile;
 
-        return bonus;
+        return miles;
     }
 }
